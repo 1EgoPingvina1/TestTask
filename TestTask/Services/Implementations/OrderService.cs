f097cb5a5126929e27fbcf3de4de233c042d545f
Andrew <sinkov407@gmail.com>
2024-05-12 13:52:02 +0300

    Add project files.

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public OrderService(ApplicationDbContext context) => _context = context;
+        
+        public async Task<Order> GetOrder()
+            => await _context.Orders.OrderByDescending(c => c.CreatedAt)
+                                    .FirstOrDefaultAsync();
+        
+        public async Task<List<Order>> GetOrders()
+            => await _context.Orders.Include(u => u.User)
+                                    .Where(s => s.User.Status == Enums.UserStatus.Active)
+                                    .OrderBy(c => c.CreatedAt)
+                                    .ToListAsync();
+        
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        public readonly ApplicationDbContext _context;
+        public UserService(ApplicationDbContext context) => _context = context;
+
+        public async Task<User> GetUser()
+            => await _context.Users.Where(o => o.Orders.Any(d => d.CreatedAt.Year == 2003)).OrderByDescending(p => p.Orders.Sum(p => p.Price)).FirstOrDefaultAsync();
+
+
+
+        public async Task<List<User>> GetUsers()
+            => await _context.Users.Where(o => o.Orders.Any(date => date.CreatedAt.Year == 2004 && date.Status == Enums.OrderStatus.Paid)).ToListAsync();
+        
+    }
+}
